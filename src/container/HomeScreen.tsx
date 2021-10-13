cdc8b6efc0bb82b3c9f3f7f29815ce08b3d40ecb
Silvio Júnior <silvio.junior@ibti.org.br>
2021-10-13 14:32:23 -0300

    Criação de Página Home

---
+++ src/container/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BoxContainer from "../components/BoxContainer/BoxContainer";
+import Header from "../components/Header/Header";
+
+const HomeScreen = () => {
+    return (
+        <BoxContainer>
+            <Header />
+        </BoxContainer>
+    );
+};
+
+export default HomeScreen;
